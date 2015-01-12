bf0b26d44fb2737a5751466d3a9fa32c34481e5c
Sohrab Khan <sohrab@myminifactory.com>
2015-01-12 15:43:22 +0000

    Finished project

---
+++ features/bootstrap/FeatureContext.php
@@ -0,0 +1,57 @@
+<?php
+
+use Behat\Behat\Context\BehatContext,
+    Behat\Behat\Exception\PendingException;
+use Behat\Gherkin\Node\PyStringNode,
+    Behat\Gherkin\Node\TableNode;
+
+/**
+ * Features context.
+ */
+class FeatureContext extends BehatContext
+{
+    /**
+     * Initializes context.
+     * Every scenario gets it's own context object.
+     *
+     * @param array $parameters context parameters (set them up through behat.yml)
+     */
+    public function __construct(array $parameters)
+    {
+        // Initialize your context here
+    }
+
+    private $output;
+
+    /** @Given /^I am in a directory "([^"]*)"$/ */
+    public function iAmInADirectory($dir)
+    {
+        if (!file_exists($dir)) {
+            mkdir($dir);
+        }
+        chdir($dir);
+    }
+
+    /** @Given /^I have a file named "([^"]*)"$/ */
+    public function iHaveAFileNamed($file)
+    {
+        touch($file);
+    }
+
+    /** @When /^I run "([^"]*)"$/ */
+    public function iRun($command)
+    {
+        exec($command, $output);
+        $this->output = trim(implode("\n", $output));
+    }
+
+    /** @Then /^I should get:$/ */
+    public function iShouldGet(PyStringNode $string)
+    {
+        if (preg_match('/' . $string . '/', $this->output)) {
+            throw new Exception(
+                "Actual output is:\n" . $this->output
+            );
+        }
+    }
+}
---
+++ features/merchant.feature
@@ -0,0 +1,15 @@
+Feature: merchant
+  In order to generate a list of transactions
+  I should have data.csv file and should supply merchant id
+  I need to be able to get the the transactions for that merchant
+
+Scenario: Display transactions for a merchant
+  Given I am in a directory "AffWindow"
+  And I have a file named "console.php"
+  And I have a file named "data.csv"
+  When I run "php console.php data.csv 2"
+  Then I should get:
+
+  """
+[2 : 02/05/2010 : EUR 12.00]
+  """
